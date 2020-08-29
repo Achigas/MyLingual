08588abb51c3b92a011dec393689b66f3359e137
Windy <lauren.euge@gmail.com>
2020-08-29 11:36:04 -0700

    update sign up form

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,6 +1,28 @@
-function signupFormHandler(event) {
+async function signupFormHandler(event) {
     event.preventDefautl();
-    
+    //const id = document.querySelector('#id-signup').value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    //const tag = document.querySelector('#tag-signup').value.trim();
+
+    if (username && email) {
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                //id,
+                username,
+                email
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        //check response status
+        if (response.ok) {
+            console.log('success');
+        } else {
+            alert(response.statusText);
+        }
+    }
 
 }
 
