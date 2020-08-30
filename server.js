22c7e78733b3c3263a9749b831b2d192cccfaf8f
LYNN DAMOTA <bdamota0506@gmail.com>
2020-08-29 17:42:40 -0700

    version for testing

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
-// app.use(routes);
+app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
