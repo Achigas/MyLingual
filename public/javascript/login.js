017cad6e4c526ea9d4fab140edcc96805c31af0e
Windy <lauren.euge@gmail.com>
2020-08-29 17:42:08 -0700

    add login handler

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,33 +1,11 @@
 async function signupFormHandler(event) {
     event.preventDefautl();
-    //const id = document.querySelector('#id-signup').value.trim();
     const username = document.querySelector('#username-signup').value.trim();
-    const password = document.querySelector()
-    //const email = document.querySelector('#email-signup').value.trim();
-
-// function langDropDown() {
-//     document.getElementById("langDropdown").classList.toggle("show");
-// }
-
-// window.onclick = function(event) {
-//     if (!event.target.matches('.dropbtn')) {
-//         var dropdowns = document.getElementsByClassName("dropdown-content");
-//         var i;
-//         for (i=0; i < dropdowns.length; i++) {
-//             var openDropdown = dropdowns[i];
-//             if (openDropdown.classList.contains ('show')) {
-//                 openDropdown.classList.remove('show');
-//             }
-//         }
-//     }
-// }
-    //const tag = document.querySelector('#tag-signup').value.trim();
-
+    const password = document.querySelector('#password-signup').value.trim();
     if (username && password) {
         const response = await fetch('/api/users', {
             method: 'post',
             body: JSON.stringify({
-                //id,
                 username,
                 password
             }),
@@ -47,5 +25,30 @@ async function signupFormHandler(event) {
 //redirect to dashbaord 
 // document.location.replace('/dashboard')
 
-
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
