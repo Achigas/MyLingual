ce66dbeb75f60550deb110c46e5673d74b2f106c
AChigas <Achigas34@gmail.com>
2020-08-31 20:44:59 -0700

    modified models and seed data

--- models/index.js
+++ models/index.js
@@ -1,33 +1,33 @@
 const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
-const Languages = require('./Languages');
+const Language = require('./Language');
 
 Post.belongsTo(User, {
-    foreignKey: 'userId',
+    foreignKey: 'user_id',
     onDelete: 'CASCADE'
 });
 
 Post.hasMany(Comment, {
-    foreignKey: 'postId',
+    foreignKey: 'post_id',
     onDelete: 'CASCADE'
 });
 
 Comment.belongsTo(User, {
-    foreignKey: 'userId',
+    foreignKey: 'user_id',
     onDelete: 'CASCADE'
 });
 
 Post.belongsTo(User, {
-    foreignKey: 'userId',
+    foreignKey: 'user_id',
     onDelete: 'CASCADE'
 });
 
-User.belongsTo(Languages, {
+User.belongsTo(Language, {
     foreignKey: 'language_id'
 });
 
-Languages.hasMany(User, {
+Language.hasMany(User, {
     foreignKey: 'language_id'
 });
 
@@ -35,5 +35,5 @@ module.exports = {
     User,
     Comment,
     Post, 
-    Languages
+    Language
 };
--- seeds/index.js
+++ seeds/index.js
@@ -1,18 +1,19 @@
-const seedCategories = require('./category-seeds');
-const seedProducts = require('./product-seeds');
-const seedTags = require('./tag-seeds');
-const seedProductTags = require('./product-tag-seeds');
+const seedUsers = require('./user-seeds');
+const seedLanguages = require('./language-seeds');
+const seedPosts= require('./post-seeds');
+const seedComments = require('./comment-seeds');
 
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
-  await seedUsers();
-  console.log('\n----- USERS SEEDED -----\n');
 
   await seedLanguages();
   console.log('\n----- LANGUAGES SEEDED -----\n');
+
+  await seedUsers();
+  console.log('\n----- USERS SEEDED -----\n');
 
   await seedPosts();
   console.log('\n----- POSTS SEEDED -----\n');
