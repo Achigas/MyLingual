d19f9bd8de20e1c3e605bede3856ace1d08b5621
Windy <lauren.euge@gmail.com>
2020-08-31 21:05:38 -0700

    add logout.js

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -2,6 +2,7 @@ async function signupFormHandler(event) {
     event.preventDefautl();
     const username = document.querySelector('#username-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
+    
     if (username && password) {
         const response = await fetch('/api/users', {
             method: 'post',
