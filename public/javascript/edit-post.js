97e099e9175f60bd4dd4e5e0170f5be2d714f1a1
Windy <lauren.euge@gmail.com>
2020-08-31 21:46:24 -0700

    add fetch to edit-post.js

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,6 +1,15 @@
 async function editFormHandler(event) {
     event.preventDefault();
 
+    await fetch('api/posts/${id}', {
+        method: 'PUT',
+        body: JSON.stringify({
+            title
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
 }
 
 document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
