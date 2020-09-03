10291e6a2488e90ca91332522c2963b9ca9c8da0
LYNN DAMOTA <bdamota0506@gmail.com>
2020-09-02 23:08:39 -0700

    different bootstrap for nav menu

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -11,7 +11,7 @@ async function newFormHandler(event) {
         body: JSON.stringify({
             title:title, 
             post_url:post_url,
-            post:post 
+            post: post
         }),
         headers: {
             'Content-Type': 'application/json'
