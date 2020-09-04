e5c88e6e98df012bbc881c1a0521cb73d53b4e24
LYNN DAMOTA <bdamota0506@gmail.com>
2020-09-04 12:28:38 -0700

    explore routes complete

--- controllers/index.js
+++ controllers/index.js
@@ -3,13 +3,15 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
-const meetRoutes = require('./meet-routes.js')
+const meetRoutes = require('./meet-routes.js');
+const exploreRoutes = require('./explore-routes.js')
  
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
-router.use('/dashboard', dashboardRoutes)
-router.use('/meet-people', meetRoutes)
+router.use('/dashboard', dashboardRoutes);
+router.use('/meet-people', meetRoutes);
+router.use('/explore', exploreRoutes)
 
 router.use((req, res) => {
   res.status(404).end();
