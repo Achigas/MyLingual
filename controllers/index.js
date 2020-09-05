acb62a6d6d278b259126ad4eeab8062fd8f33140
LYNN DAMOTA <bdamota0506@gmail.com>
2020-09-05 10:03:44 -0700

    privacy policy and terms and conditions added

--- controllers/index.js
+++ controllers/index.js
@@ -4,14 +4,14 @@ const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
 const meetRoutes = require('./meet-routes.js');
-const exploreRoutes = require('./explore-routes.js')
+const exploreRoutes = require('./explore-routes.js');
  
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/meet-people', meetRoutes);
-router.use('/explore', exploreRoutes)
+router.use('/explore', exploreRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
